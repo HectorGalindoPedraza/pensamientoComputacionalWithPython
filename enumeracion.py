68ad73396d065e8968cdedf90a76c9659e73cdb7
Hécotr Galindo <hec.gph@gmail.com>
2022-03-30 00:24:14 -0500

    Enumeracion exhaustiva + mi mini proyecto (mostrar tabla del {tabla})

---
+++ enumeracion.py
@@ -0,0 +1,25 @@
+# objetivo = int(input('Escoge un entero: '))
+#respuesta = 0
+
+#while respuesta**2 < objetivo:
+#    print(respuesta)
+#    respuesta += 1
+#if respuesta**2 == objetivo:
+#    print(f'La raiz cuadrada de {objetivo} es {respuesta}')
+#else:
+#    print(f'{objetivo} no tiene una raiz cuadrada exacta')
+
+
+
+# MI MINI PROYECTO DE LA CLASE
+
+# tabla = int(input('Que tabla de multiplicar quieres ver?: '))
+# fin = int(input('Hasta que número quieres ver la tabla?: '))
+# cont = 0
+
+# while cont <= fin:
+#     igual = tabla * cont
+#     print(f'{tabla} x {cont} = {igual}')
+#     cont += 1
+
+
